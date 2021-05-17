e70eab47b7d5a2e559b7dac62a7d40bd5d46f591
M-java86 <mpythondata@outlook.com>
2021-05-17 17:53:21 -0400

    MO: commit -m calculator re-visit, add/sub/*/div/%-modulus

--- src/com/qa/java/Calculator.java
+++ src/com/qa/java/Calculator.java
@@ -1,9 +1,25 @@
 package com.qa.java;
 
-public class Calculator {
+public class Calculator { 
 	
-	public static int add(int x, int y) {
-		return x + y;
+	public static int add(int num1, int num2) {
+		return num1 + num2;
 	}
+	public static int sub(int num1, int num2) {
+		return num1 - num2;
+	}
+	public static int mult(int num1 , int num2) {
+		return num1 * num2;
+	}
+	// no floating point value!!!
+	public static int div(int num1, int num2) {
+		return num1 / num2;
+	}
+	// should be okay 
+	public static int mod(int num1 , int num2) {
+	return num1 % num2;
+	
+	}
+
 
 }
--- src/com/qa/java/Runner.java
+++ src/com/qa/java/Runner.java
@@ -11,18 +11,28 @@ public class Runner {
 	// Member - Method has a Scope
 	public static void main(String[] args) {
 		
+		int var = 5;
+		
+		var -= 3;
+		
+		System.out.println(var);
+		
 		//int result = 2;
 		
 		//result -=8; // we can change the value of the variable by using ++;
 		
 		
 		//int result = Calculator.add(5, 5);
-			boolean flag = true;
+		//	boolean flag = true;
 		
-		System.out.println(!flag);//-we can change the logic of this statement by add !, which is called logical not argument...
+		//System.out.println(!flag);//-we can change the logic of this statement by add !, which is called logical not argument...
 		//..this is going imply the opposite of the statement here. So if it's true, then it will be false and vice versa.
 		//This is only used in conjunction with a boolean. 
 		
 	}
+	
+		public static int add(int num1, int num2) {
+			return num1 + num2;
+		}
 
 }
