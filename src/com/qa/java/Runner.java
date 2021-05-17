3ca30b671329d17bc6077c37f44b17644899b1f3
M-java86 <mpythondata@outlook.com>
2021-05-16 21:22:48 -0400

    MO: Developer branch Runner.java added variable and boolean functions

--- src/com/qa/java/Runner.java
+++ src/com/qa/java/Runner.java
@@ -2,11 +2,25 @@ package com.qa.java;
 
 public class Runner {
 	
+	// Member -attributes
+	public static int id;
+	public static String msg = "Hello World";
+	
+	
+	// Member - Method has a Scope
 	public static void main(String[] args) {
 		
-		int result = Calculator.add(5, 5);
+		//int result = 2;
 		
-		System.out.println(result);
+		//result -=8; // we can change the value of the variable by using ++;
+		
+		
+		//int result = Calculator.add(5, 5);
+			boolean flag = true;
+		
+		System.out.println(!flag);//-we can change the logic of this statement by add !, which is called logical not argument...
+		//..this is going imply the opposite of the statement here. So if it's true, then it will be false and vice versa.
+		//This is only used in conjunction with a boolean. 
 		
 	}
 
