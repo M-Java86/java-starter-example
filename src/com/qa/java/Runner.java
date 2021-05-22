610973c2be2c7a2aaaeb99485518976c2d06fb48
M-java86 <mpythondata@outlook.com>
2021-05-22 19:29:14 -0400

    MO: Added Task instructions

--- src/com/qa/java/Runner.java
+++ src/com/qa/java/Runner.java
@@ -16,32 +16,29 @@ public class Runner {
 	}
 	
 }     
-//System.out.print(Calculator.div(5, 2));
-		
-		//int var = 5;
-		
-		//var += 3;
-		
-		//System.out.println(var);
-		
-		//int result = 2;
-		
-		//result -=8; // we can change the value of the variable by using ++;
-		
-		
-		//int result = Calculator.add(5, 5);
-		//	boolean flag = true;
-		
-		//System.out.println(!flag);//-we can change the logic of this statement by add !, which is called logical not argument...
-		//..this is going imply the opposite of the statement here. So if it's true, then it will be false and vice versa.
-		//This is only used in conjunction with a boolean. 
-		
-	//}
-	
-		//public static int add(int num1, int num2) {
-			//return num1 + num2;
-		//}
 
-//} 
+// This task is part of QA Community: Java Beginner -> Operators -> Exercises.
+
+//1). Create a method that accepts two integers as input, 
+//then returns an integer that is the sum of these two parameters. 
+//(Addition) 
+
+
+//2). Create the following additional three methods that each take two parameters:
+//--//-Multiplication - which takes two numbers and returns the product.
+//-Subtraction - which takes two numbers, then returns the result of the subtraction.
+//-Division - which takes two numbers, then returns the result of the division.
+
+//Your division method may have returned the wrong result; This is called a rounding error..
+//and is quite common in most languages. 
+//this is because we were using ints rather than doubles.
+
+//---3). Modify the division method so that it takes Double parameters 
+//and then return a Double, if not already.
+
+//--- Some helpful short-hands for Eclipse:
+
+//type - "syso"  + ctrl + space to generate the  System.out.println() method.
+	//ctrl + shift + f - auto-format/indent
 
 
