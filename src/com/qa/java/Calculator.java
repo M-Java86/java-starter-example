efc8497c3626787ec02536faf5be0ad59db6f7a6
M-java86 <mpythondata@outlook.com>
2021-05-24 15:55:09 -0400

    MO: added junit Test

--- src/com/qa/java/Calculator.java
+++ src/com/qa/java/Calculator.java
@@ -2,20 +2,20 @@ package com.qa.java;
 
 public class Calculator { 
 	
-	//public static double add(double num1, double num2) {
-		//return num1 + num2;
-	//}
-	//public static double sub(double num1, double num2) {
-		//return num1 - num2;
-	//}
+	public static double add(double num1, double num2) {
+		return num1 + num2;
+	}
+	public static double sub(double num1, double num2) {
+		return num1 - num2;
+	}
 	public static double mult(double num1 , double num2) {
 		return num1 * num2;
 	}
-	// no floating point value!!!
+	 //no floating point value!!!
 	public static double div(double num1, double num2) {
 		return num1 / num2;
 	}
-	// should be okay 
+	//should be okay 
 	public static double mod(double num1 , double num2) {
 	return num1 % num2;
 	
